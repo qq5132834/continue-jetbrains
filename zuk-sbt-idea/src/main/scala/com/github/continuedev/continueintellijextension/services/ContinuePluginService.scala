e5431abf1bf8c12890816bb06a1880497c4df904
qq5132834 <513283439@qq.com>
2025-03-28 19:33:58 +0800

    添加注释

---
+++ zuk-sbt-idea/src/main/scala/com/github/continuedev/continueintellijextension/services/ContinuePluginService.scala
@@ -0,0 +1,14 @@
+package com.github.continuedev.continueintellijextension.services
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.DumbAware
+
+@Service(Service.Level.PROJECT)
+class ContinuePluginService extends Disposable {
+
+  override def dispose(): Unit = {
+
+  }
+
+}
--- zuk-sbt-idea/src/main/scala/com/github/continuedev/continueintellijextension/toolWindow/ContinueBrowser.scala
+++ zuk-sbt-idea/src/main/scala/com/github/continuedev/continueintellijextension/toolWindow/ContinueBrowser.scala
@@ -1,11 +1,19 @@
 package com.github.continuedev.continueintellijextension.toolWindow
 
-import com.github.continuedev.continueintellijextension.services.ContinueExtensionSettings
+import com.github.continuedev.continueintellijextension.activities.ContinuePluginDisposable
+import com.github.continuedev.continueintellijextension.constants.MessageTypes
+import com.github.continuedev.continueintellijextension.services.{ContinueExtensionSettings, ContinuePluginService}
+import com.google.gson.{Gson, JsonObject, JsonParser}
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.project.Project
-import com.intellij.ui.jcef.JBCefBrowser
+import com.intellij.openapi.util.Disposer
+import com.intellij.ui.jcef.{JBCefBrowser, JBCefBrowserBase, JBCefJSQuery}
 import org.cef.CefApp
+import org.cef.browser.CefBrowser
 import org.jetbrains.scala.samples.ui.CustomSchemeHandlerFactory
+
+import java.util
+import java.util.UUID
 
 class ContinueBrowser(val project: Project, url: String) {
 
@@ -17,11 +25,100 @@ class ContinueBrowser(val project: Project, url: String) {
     )
   }
 
-  val browser: JBCefBrowser = null
+  var browser: JBCefBrowser = null
 
   def init() = {
     val isOSREnabled = ServiceManager.getService(classOf[ContinueExtensionSettings]).continueState.enableOSR
 
+    this.browser = JBCefBrowser.createBuilder().setOffScreenRendering(isOSREnabled).build()
+
+    registerAppSchemeHandler()
+    browser.loadURL(url)
+    Disposer.register(ContinuePluginDisposable.getInstance(this.project), this.browser)
+
+    // Listen for events sent from browser
+    val myJSQueryOpenInBrowser = JBCefJSQuery.create(browser.asInstanceOf[JBCefBrowserBase])
+
+    myJSQueryOpenInBrowser.addHandler((msg: String) => {
+      val parser = new JsonParser()
+      val json: JsonObject = parser.parse(msg).getAsJsonObject
+      val messageType = json.get("messageType").getAsString
+      val data = json.get("data")
+      val messageId = json.get("messageId").getAsString
+
+      val continuePluginService = ServiceManager.getService(
+        this.project,
+        classOf[ContinuePluginService]
+      )
+
+      val respond = (data: AnyRef) => {
+        sendToWebview(messageType, data, if(messageId!=null) messageId else uuid())
+      }
+
+      if (MessageTypes.PASS_THROUGH_TO_CORE.contains(messageType)) {
+//        continuePluginService.coreMessenger?.request(messageType, data, messageId, respond)
+//        return@addHandler null
+      }
+
+      // If not pass through, then put it in the status/content/done format for webview
+      // Core already sends this format
+      val respondToWebview = (data: AnyRef) => {
+        val map = scala.collection.mutable.HashMap[String, AnyRef]()
+        map.put("status" , "success")
+        map.put("content" , data)
+        map.put("done" , true)
+        sendToWebview(messageType, map, if( messageId!=null) messageId else uuid())
+
+      }
+
+      if(msg!=null){
+        //continuePluginService.ideProtocolClient?.handleMessage(msg, respondToWebview)
+      }
+
+      null
+    })
+  }
+
+  def executeJavaScript(browser: CefBrowser, myJSQueryOpenInBrowser: JBCefJSQuery) = {
+    // Execute JavaScript - you might want to handle potential exceptions here
+    val script = """window.postIntellijMessage = function(messageType, data, messageId) {
+                const msg = JSON.stringify({messageType, data, messageId});
+                ${myJSQueryOpenInBrowser.inject("msg")}
+            }""".trim
+    if(browser!=null){
+      browser.executeJavaScript(script, browser.getURL, 0)
+    }
+  }
+
+  def sendToWebview(messageType: String,
+                    data: AnyRef,
+                    messageId: String = uuid()
+                   ): Unit = {
+    val map = scala.collection.mutable.HashMap[String, AnyRef]()
+    map.put("messageId", messageId)
+    map.put("messageType", messageType)
+    map.put("data", data)
+
+    val jsonData = new Gson().toJson(map)
+
+    val jsCode = this.buildJavaScript(jsonData)
+
+    try {
+//      this.browser.executeJavaScriptAsync(jsCode).onError {
+//        println("Failed to execute jsCode error: ${it.message}")
+//      }
+    }
+    catch {
+      case error: IllegalStateException => println(s"Webview not initialized yet ${error}")
+    }
+  }
+
+  private def buildJavaScript(jsonData: String): String = {
+    return """window.postMessage($jsonData, "*");"""
+  }
+
+  def uuid(): String = {
+    UUID.randomUUID().toString
   }
 
 }
