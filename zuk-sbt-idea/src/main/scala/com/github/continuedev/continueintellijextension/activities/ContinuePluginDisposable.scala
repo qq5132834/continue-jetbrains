cab16a3849061051e71c242133a3582a24842861
qq5132834 <513283439@qq.com>
2025-03-28 18:16:04 +0800

    添加注释

---
+++ zuk-sbt-idea/src/main/scala/com/github/continuedev/continueintellijextension/activities/ContinuePluginDisposable.scala
@@ -0,0 +1,33 @@
+package com.github.continuedev.continueintellijextension.activities
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+
+/**
+ * The service is a parent disposable that represents the entire plugin lifecycle
+ * and is intended to be used instead of the project/application as a parent disposable,
+ * ensures that disposables registered using it as parents will be processed when the plugin is unloaded to avoid memory leaks.
+ *
+ * @author lk
+ */
+@Service(Service.Level.APP, Service.Level.PROJECT)
+class ContinuePluginDisposable extends Disposable {
+
+  override def dispose(): Unit = {
+
+  }
+}
+
+object ContinuePluginDisposable {
+
+  def getInstance(): ContinuePluginDisposable = {
+    ApplicationManager.getApplication().getService(classOf[ContinuePluginDisposable])
+  }
+
+  def getInstance(project: Project): ContinuePluginDisposable = {
+    project.getService(classOf[ContinuePluginDisposable])
+  }
+
+}
