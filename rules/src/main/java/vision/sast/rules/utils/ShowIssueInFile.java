582c159f42fc8afda480eb21005a8abc62bda1c3
qq5132834 <513283439@qq.com>
2025-04-01 19:14:16 +0800

    webgi

--- rules/src/main/java/vision/sast/rules/utils/ShowIssueInFile.java
+++ rules/src/main/java/vision/sast/rules/utils/ShowIssueInFile.java
@@ -12,10 +12,37 @@ public class ShowIssueInFile {
     public static String show(String fileName, List<IssueDto> dtoList) throws Exception {
         List<String> lines = FileUtils.readLines(new File(fileName), Charset.forName("utf-8"));
         StringBuilder sb = new StringBuilder();
+        int i = 1;
         for (String line : lines) {
-            sb.append(line).append("<br>");
+//            line.replaceAll(" ", "");
+//            line = "<div>" + line + "</div>";
+            //line = line.replaceAll("<", "&lt;").replaceAll(">", "&gt;");//.replaceAll(" ", "&nbsp;");
+            sb.append(line);
+            sb.append("<br>");
+            i++;
         }
-        return sb.toString();
+//        return sb.toString();
+        return "<html>" +
+                "<head>" +
+//                 "<link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css' rel='stylesheet'>" +
+//                 "<script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js'></script>" +
+                 "</head>" +
+                "<body>" +
+                "<pre><code class='language-clike'>" +
+                sb.toString() +
+                 "</code></pre>" +
+                "</body>" +
+                "</html>";
     }
+
+
+    /***
+     * return "<html><head>" +
+     *            "<link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css' rel='stylesheet'>" +
+     *            "<script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js'></script>" +
+     *            "</head><body><pre><code class='language-" + language + "'>" +
+     *            content +
+     *            "</code></pre></body></html>";
+     */
 
 }
