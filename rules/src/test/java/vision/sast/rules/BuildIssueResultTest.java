bdb17b9034c407faf312ed39a6e5d4db3f97c3f5
qq5132834 <513283439@qq.com>
2025-04-01 20:18:35 +0800

    webgi

--- rules/src/test/java/vision/sast/rules/BuildIssueResultTest.java
+++ rules/src/test/java/vision/sast/rules/BuildIssueResultTest.java
@@ -1,9 +1,5 @@
 package vision.sast.rules;
 
-import vision.sast.rules.dto.IssueResult;
-
-import java.io.File;
-
 public class BuildIssueResultTest {
     public static void main(String[] args) {
         String[] args1 = {"src/test/resources/issue.json"};
