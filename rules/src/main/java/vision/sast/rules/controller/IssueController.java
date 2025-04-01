2b1021c6fa756aed3eee039b9d663b19ced81d1d
qq5132834 <513283439@qq.com>
2025-04-01 17:00:03 +0800

    webgi

--- rules/src/main/java/vision/sast/rules/controller/IssueController.java
+++ rules/src/main/java/vision/sast/rules/controller/IssueController.java
@@ -1,5 +1,8 @@
 package vision.sast.rules.controller;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,23 @@ public class IssueController {
     @GetMapping("")
     public  String list(){
         return "index";
+    }
+
+
+    @GetMapping("md")
+    public String md(){
+        String md = "# 标题1\n" +
+                "## 标题2\n" +
+                "```java\n" +
+                "String s = \"hello world.\";\n" +
+                "你好，代码。\n" +
+                "```\n";
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(md);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+
     }
 
 
