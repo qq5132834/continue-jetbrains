a26958bb87e9f625296220bac243b51744ae1096
qq5132834 <513283439@qq.com>
2025-04-02 14:30:12 +0800

    commit

---
+++ rules/src/test/java/vision/sast/rules/ShowIssueInFileTest.java
@@ -0,0 +1,16 @@
+package vision.sast.rules;
+
+import org.apache.commons.io.FileUtils;
+import vision.sast.rules.dto.IssueDto;
+import vision.sast.rules.utils.ShowIssueInFile;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ShowIssueInFileTest {
+    public static void main(String[] args) throws Exception {
+        String str = ShowIssueInFile.show("src/test/resources/test.c", new ArrayList<IssueDto>());
+        System.out.println(str);
+        FileUtils.writeStringToFile(new File("src/test/resources/test.html"), str, "UTF-8");
+    }
+}
