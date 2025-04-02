18ccd9da930b37f55b73ca23e58ae639c49842c6
qq5132834 <513283439@qq.com>
2025-04-02 14:35:48 +0800

    commit

--- rules/src/main/java/vision/sast/rules/utils/ShowIssueInFile.java
+++ rules/src/main/java/vision/sast/rules/utils/ShowIssueInFile.java
@@ -11,58 +11,54 @@ import java.util.stream.Collectors;
 
 public class ShowIssueInFile {
 
-    public static String show(String fileName, List<IssueDto> dtoList) throws Exception {
-        List<IssueDto> sortedList = dtoList.stream().sorted(Comparator.comparing(IssueDto::getLine)).toList();
-        List<String> lines = FileUtils.readLines(new File(fileName), Charset.forName("utf-8"));
-        StringBuilder sb = new StringBuilder("<ol>");
+    public static String show(String fileName, List<IssueDto> dtoList) {
+        try {
+            List<IssueDto> sortedList = dtoList.stream().sorted(Comparator.comparing(IssueDto::getLine)).toList();
+            List<String> lines = FileUtils.readLines(new File(fileName), Charset.forName("utf-8"));
+            StringBuilder sb = new StringBuilder("<ol>");
 
-        List<String> newLines = lines.stream().map(line->{
-            line = "<li>" +line + "</li>";
-            return line;
-        }).collect(Collectors.toList());
+            List<String> newLines = lines.stream().map(line->{
+                line = "<li>" +line + "</li>";
+                return line;
+            }).collect(Collectors.toList());
 
-        int insertTime = 0;
-        for (IssueDto dto: sortedList) {
-            int line = dto.getLine();
-            int index = line + insertTime;
-            if(index > 0){
-                String divStr = "<div style='background-color: pink'>"
-                        + dto.getName() + "<br>"
-                        + dto.getLine() + "/" + dto.getVtId() + "/" + dto.getRule() + "/" + dto.getDefectLevel() + "/" + dto.getDefectType()  + "/" + "<br>"
-                        + dto.getRuleDesc() + "<br>"
-                        + "</div>";
-                newLines.add(index, divStr);
-                insertTime++;
+            int insertTime = 0;
+            for (IssueDto dto: sortedList) {
+                int line = dto.getLine();
+                int index = line + insertTime;
+                if(index > 0){
+                    String divStr = "<div style='background-color: pink'>"
+                            + dto.getName() + "<br>"
+                            + dto.getLine() + "/" + dto.getVtId() + "/" + dto.getRule() + "/" + dto.getDefectLevel() + "/" + dto.getDefectType()  + "/" + "<br>"
+                            + dto.getRuleDesc() + "<br>"
+                            + "</div>";
+                    newLines.add(index, divStr);
+                    insertTime++;
+                }
             }
-        }
 
-        for (String line : newLines) {
-            sb.append(line);
-            sb.append("<br>");
-        }
+            for (String line : newLines) {
+                sb.append(line);
+                sb.append("<br>");
+            }
 
-        sb.append("</ol>");
-        return "<html>" +
-                "<head>" +
+            sb.append("</ol>");
+            return "<html>" +
+                    "<head>" +
 //                 "<link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css' rel='stylesheet'>" +
 //                 "<script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js'></script>" +
-                 "</head>" +
-                "<body>" +
-                "<pre><code class='language-clike'>" +
-                sb.toString() +
-                 "</code></pre>" +
-                "</body>" +
-                "</html>";
-    }
+                    "</head>" +
+                    "<body>" +
+                    "<pre><code class='language-clike'>" +
+                    sb.toString() +
+                    "</code></pre>" +
+                    "</body>" +
+                    "</html>";
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 
-
-    /***
-     * return "<html><head>" +
-     *            "<link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css' rel='stylesheet'>" +
-     *            "<script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js'></script>" +
-     *            "</head><body><pre><code class='language-" + language + "'>" +
-     *            content +
-     *            "</code></pre></body></html>";
-     */
 
 }
