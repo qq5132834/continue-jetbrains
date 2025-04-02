54e1621ab6f5812ac9f98199ea33ada587a79fa0
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-03 00:24:41 +0800

    commit

--- rules/src/main/java/vision/sast/rules/controller/SourceCodeController.java
+++ rules/src/main/java/vision/sast/rules/controller/SourceCodeController.java
@@ -3,24 +3,39 @@ package vision.sast.rules.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vision.sast.rules.RulesApplication;
 import vision.sast.rules.dto.IssueDto;
 import vision.sast.rules.utils.ShowIssueInFile;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class SourceCodeController {
 
+    private static Map<String, List<IssueDto>> issuesMap = new ConcurrentHashMap<>();
+
     @GetMapping("sourceCode")
     public String fileAndVtid(String vtid, String file) {
-        FileController.loadInitList();
-        try {
-            List<IssueDto> dtos = FileController.fileIssuesMap.get(file).stream().filter(issueDto -> issueDto.getVtId().equals(vtid)).toList();
-            return ShowIssueInFile.show(file, dtos);
-        }catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
+        if (vtid == null || file == null) {
+            try {
+                String key = vtid + ":" + file;
+                if(issuesMap.get(key)==null){
+                    List<IssueDto> issueDtos = RulesApplication.ISSUE_RESULT.getResult().stream().filter(dto->dto.getFilePath().equals(file) && dto.getVtId().equals(vtid)).toList();
+                    issuesMap.put(key, issueDtos);
+                }
+                List<IssueDto> issueDtos = issuesMap.get(key);
+                return ShowIssueInFile.show(file, issueDtos);
+            }catch (Exception e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
         }
+        else {
+            return "null";
+        }
+
     }
 
 }
