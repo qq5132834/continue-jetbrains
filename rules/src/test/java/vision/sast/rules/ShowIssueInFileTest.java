ac7ed9d56ee34b8591e215c4f887aad746de4fdb
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-03 09:57:35 +0800

    新增utf-8、ANSI格式文件

--- rules/src/test/java/vision/sast/rules/ShowIssueInFileTest.java
+++ rules/src/test/java/vision/sast/rules/ShowIssueInFileTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 public class ShowIssueInFileTest {
     public static void main(String[] args) throws Exception {
+        RulesApplication.loadProperties();
         String str = ShowIssueInFile.show("src/test/resources/ANSI_test.c", new ArrayList<IssueDto>());
         System.out.println(str);
     }
