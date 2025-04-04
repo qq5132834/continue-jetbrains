a5fdd6664811d37ea04f2ecaf8eda0f6bba4164f
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-04 12:53:49 +0800

    不在保存html文件

--- rules/src/test/java/vision/sast/rules/CodeCatEditor.java
+++ rules/src/test/java/vision/sast/rules/CodeCatEditor.java
@@ -3,24 +3,38 @@ package vision.sast.rules;
 import java.awt.*;
 import javax.swing.*;
 import org.fife.ui.rsyntaxtextarea.*;
+import org.fife.ui.rsyntaxtextarea.modes.PlainTextTokenMaker;
 import org.fife.ui.rtextarea.*;
 
 public class CodeCatEditor {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(CodeCatEditor::createAndShowGUI);
+
+        SwingUtilities.invokeLater(()->{
+            String code = getCatCode();
+            CodeCatEditor.createAndShowGUI(code);
+        });
+
+        TokenMaker tokenMaker;
+        TokenMakerFactory tokenMakerFactory;
+        PlainTextTokenMaker plainTextTokenMaker;
+
     }
 
-    private static void createAndShowGUI() {
+    public static void createAndShowGUI(String text) {
         JFrame frame = new JFrame("Code Cat Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(800, 500);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - frame.getWidth()) / 2;
+        int y = (screenSize.height - frame.getHeight()) / 2;
+        frame.setLocation(x, y);
 
         // 创建 RSyntaxTextArea
         RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
-        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS);
         textArea.setCodeFoldingEnabled(true);
-        textArea.setText(getCatCode());
+        textArea.setText(text);
 
         // 将 RSyntaxTextArea 包装在 RTextScrollPane 中
         RTextScrollPane sp = new RTextScrollPane(textArea);
@@ -33,13 +47,11 @@ public class CodeCatEditor {
     private static String getCatCode() {
         return """
                #include <stdio.h>
-               int main()
-               {
-                  int a = 0;
-                  int b = 2;
-                  int c = a + b;
-                  printf("Hello, World! %d", c);
-                  return 0;
+               using namespace std;
+                              
+               int main() {
+                   cout << "Hello, C++!" << endl;
+                   return 0;
                }
                """;
     }
