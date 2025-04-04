6a6481f7e509c68f0bd65704119511c8f32c8f2d
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-04 18:42:20 +0800

    高亮

--- rules/src/test/java/vision/sast/rules/SourceCodeUtilTest.java
+++ rules/src/test/java/vision/sast/rules/SourceCodeUtilTest.java
@@ -5,6 +5,9 @@ import vision.sast.rules.utils.SourceCodeUtil;
 
 import java.util.ArrayList;
 
+/***
+ * 解析 ANSI 编码格式、UTF-8 编码格式的 文件
+ */
 public class SourceCodeUtilTest {
     public static void main(String[] args) throws Exception {
         RulesApplication.loadProperties();
