5880836342a02bdf21d35e0e3af1ac65dea9065d
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-04 14:06:19 +0800

    编辑

--- rules/src/test/java/vision/sast/rules/RulesApplicationTest.java
+++ rules/src/test/java/vision/sast/rules/RulesApplicationTest.java
@@ -1,13 +1,11 @@
 package vision.sast.rules;
 
-import org.apache.commons.io.FileUtils;
 import vision.sast.rules.dto.IssueDto;
 import vision.sast.rules.utils.ShowIssueInFile;
 
-import java.io.File;
 import java.util.ArrayList;
 
-public class ShowIssueInFileTest {
+public class RulesApplicationTest {
     public static void main(String[] args) throws Exception {
         RulesApplication.loadProperties();
         String str = ShowIssueInFile.show("src/test/resources/ANSI_test.c", new ArrayList<IssueDto>());
